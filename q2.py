e8c451824e70ed12c7d0c8b6a6e8c56cecdf995e
arjunamar5 <145699985+arjunamar5@users.noreply.github.com>
2024-07-29 22:18:06 +0530

    q2.py

--- q2.py
+++ q2.py
@@ -1,16 +1,18 @@
-def r(list,a):
-    if(a < 3):
-        return "not possible"
-    else:
-        for i in range(0,a):
-            ele = int(input())
-            list.append(ele)
-
-        a = max(list)
-        b = min(list)
-        return a-b
-
-a = int(input("enter the no of elements in the list :"))
-list = []
-
-print(r(list,a))
+def fun2(a,n):
+    if(n==3):
+        return "Range determination not possible"
+    for i in range(n):
+        ele=float(input())
+        a.append(ele)
+    max=a[0]
+    for i in range(n):
+        if(max<a[i]):
+            max=a[i]
+    min=a[0]
+    for i in range(n):
+       if(min>a[i]):
+            min=a[i]
+    return max-min
+a=[]
+n=int(input("Enter the number of elements in array"))
+print(fun2(a,n))

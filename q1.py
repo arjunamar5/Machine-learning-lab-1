fb0fe7572499b8764348fc076bdebf526072bf16
arjunamar5 <145699985+arjunamar5@users.noreply.github.com>
2024-07-29 22:13:09 +0530

    q1.py

--- q1.py
+++ q1.py
@@ -1,19 +1,10 @@
-def count_pairs_with_sum(arr, t):
-    c = 0
-    s = set()
-    
-    for number in arr:
-        complement = t - number
-        if complement in s:
-            c += 1
-        s.add(number)
-    
-    return c
-
-# Given list
-arr = [2, 7, 4, 1, 3, 6]
-t = 10
-
-# Count pairs
-pair_count = count_pairs_with_sum(arr, t)
-print(f"Number of pairs with sum {t}: {pair_count}")
+list=[2, 7, 4, 1, 3, 6]
+def fun1(list):
+    count=0                                  
+    for i in list:
+        for j in list:
+            if((i+j)==10):
+                print(i,j)
+                count=count+1
+    return count
+print(fun1(list))
